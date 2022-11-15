fae323b362e59d5f4871ac08f3b170609cd3acb5
Renbow <108724449+renbowxd@users.noreply.github.com>
2022-11-15 19:04:25 +0300

    Create thx.jsx

---
+++ pages/thx.jsx
@@ -0,0 +1,17 @@
+export default function Tos() {
+  return (
+    <div>
+     <h2 className="mt-4 text-white font-bold text-3xl">Thanks...</h2>
+      <p className="text-sm text-white/30 mb-1">Elbowizm</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+<p>⊳Thank you for choosing our bot.</p>
+<div className="py-3"></div>
+<p>⊳Flaxis Support Server : https://discord.gg/nUdr5CDZs5</p>
+<div className="py-3"></div>
+<p>⊳Producers</p>
+<div className="py-3"></div>
+<p>⊳Renbow#0210,фитон#3252,elrond#0210</p>
+</div>
+    </div>
+  );
+}
