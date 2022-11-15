9282f755a99af6b33dd4e246f32a5299e9b01ea3
Renbow <108724449+renbowxd@users.noreply.github.com>
2022-11-15 22:17:25 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,7 @@ export default function Index() {
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://bit.ly/3PHDjyC"}>
+            <Link href={"https://discord.com/oauth2/authorize?client_id=1041678362290163743&scope=bot&permissions=8&redirect_uri=https%3A%2F%2Fflaxis.tk%2Fthx"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://bit.ly/3LYzaDe"}>
+  <Link href={"https://discord.com/oauth2/authorize?client_id=1041678362290163743&scope=bot&permissions=8&redirect_uri=https%3A%2F%2Fflaxis.tk%2Fthx"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Flaxis</a>
   </Link>
